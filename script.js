b42e2ba8cb17f962062196232ca7916b4e44d2cf
Rayssa <159145255+rayssaemaria@users.noreply.github.com>
2024-06-24 08:36:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+{
+enunciado: "Voce acredita que a inteligência artificial pode tornar tarefas do dia a dia mais fáceis e eficientes?",
+alternativas: [
+"Sim",
+"Nao"
+]
+},
+{
+enunciado: "A inteligência artificial pode acabar com os empregos?",
+alternativas: [
+"Sim",
+"Nao"
+]
+},
+{
+enunciado: "Você se preocupa com questões ética relaciondas ao uso da inteligência artificial em diferentes aspectos da vida cotidiana?",
+alternativas: [
+"Sim",
+"Nao"
+]
+},
+{
+enunciado: "Pergunta 4",
+alternativas: [
+"Alternativa 1",
+"Alternativa 2"
+]
+},
+{
+enunciado: "Pergunta 5",
+alternativas: [
+"Alternativa 1",
+"Alternativa 2"
+]
+}
+];
